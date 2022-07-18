f8b84f81d2d62bbc45b9a4aae4eba47e96dd698b
marcos-asdes <marco.engc@poli.ufrj.br>
2022-07-18 05:13:53 -0300

    refactor: rebuild index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,30 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, json } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import "express-async-errors";
+
+//import ErrorHandler from './events/AppError';
+//import AppLog from './events/AppLog';
+
+//import router from "./routes/index"
+
+dotenv.config({ path: '.env' });
 
 const app = express();
-const port = 5000;
+app.use(cors());
+app.use(json());
+app.use(helmet());
+//app.use(router);
+//app.use(ErrorHandler);
+
+const PORT = +process.env.PORT || 5000;
 
 app.get("/", (_req: Request, res: Response) => {
     res.send("Online");
 });
 
-app.listen(port, () => {
-    console.log("Server listening on port " + port);
+app.listen(PORT, () => {
+    //AppLog('Server', `Server listening on port ${PORT}`);
+    console.log("Server listening on port " + PORT);
 });
